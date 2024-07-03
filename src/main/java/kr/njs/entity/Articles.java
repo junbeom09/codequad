2fc9de0589f23c941eb625c8a7a5168d44ed4c92
smhrd <qoddkfdl@gmail.com>
2024-07-03 11:05:27 +0900

    Articles 클래스 수정

--- src/main/java/kr/njs/entity/Articles.java
+++ src/main/java/kr/njs/entity/Articles.java
@@ -10,7 +10,7 @@ public class Articles {
 
     private String at_title; // 기사 제목
     private String at_content; //  기사 내용
-    private String at_pic; // 기사 사진 경로
+    private String at_url; // 기사 경로
     private String at_created; // 기사 작성일
-    private String at_idname; // 기사 작성자 이름
+    private String at_company; // 언론사 이름
 }
