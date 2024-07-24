723a22a4cd7650d78d1a41ebcddc2901b0bbb9b6
smhrd <qoddkfdl@gmail.com>
2024-07-24 11:10:33 +0900

    로그인 수정

--- src/main/newjeansfront/src/webapp/Components/Search.jsx
+++ src/main/newjeansfront/src/webapp/Components/Search.jsx
@@ -6,8 +6,142 @@ const Search = () => {
         <div>
             <main>
                 <section className="search-results" id="search-results">
-                    <div className="left-column"></div>
-                    <div className="right-column"></div>
+                    <div className="left-column">
+                        <div className="box-container">
+                            <div className="article-container">
+                                <div className="content">
+                                    <h2>기사 제목 1</h2>
+                                    <p>기사 내용 1</p>
+                                    <span className="source"><strong>KBS</strong></span>
+                                    <span className="date">2024/07/03</span>
+                                </div>
+                                <img src="https://image.wyyyes.com/svc/openChat/NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g/thumbnail/559947618_NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g_f7beddce-af7c-45b2-a17d-7c44fafcbae1.jpg" alt="기사 이미지"/>
+                            </div>
+                        </div>
+                        <div className="box-container">
+                            <div className="article-container">
+                                <div className="content">
+                                    <h2>기사 제목 2</h2>
+                                    <p>기사 내용 2</p>
+                                    <span className="source"><strong>MBC</strong></span>
+                                    <span className="date">2024/07/03</span>
+                                </div>
+                                <img src="https://image.wyyyes.com/svc/openChat/NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g/thumbnail/559947618_NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g_f7beddce-af7c-45b2-a17d-7c44fafcbae1.jpg" alt="기사 이미지"/>
+                            </div>
+                        </div>
+                        <div className="box-container">
+                            <div className="article-container">
+                                <div className="content">
+                                    <h2>기사 제목 3</h2>
+                                    <p>기사 내용 3</p>
+                                    <span className="source"><strong>SBS</strong></span>
+                                    <span className="date">2024/07/03</span>
+                                </div>
+                                <img src="https://image.wyyyes.com/svc/openChat/NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g/thumbnail/559947618_NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g_f7beddce-af7c-45b2-a17d-7c44fafcbae1.jpg" alt="기사 이미지"/>
+                            </div>
+                        </div>
+                        <div className="box-container">
+                            <div className="article-container">
+                                <div className="content">
+                                    <h2>기사 제목 4</h2>
+                                    <p>기사 내용 4</p>
+                                    <span className="source"><strong>YTN</strong></span>
+                                    <span className="date">2024/07/03</span>
+                                </div>
+                                <img src="https://image.wyyyes.com/svc/openChat/NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g/thumbnail/559947618_NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g_f7beddce-af7c-45b2-a17d-7c44fafcbae1.jpg" alt="기사 이미지"/>
+                            </div>
+                        </div>
+                        <div className="box-container">
+                            <div className="article-container">
+                                <div className="content">
+                                    <h2>기사 제목 5</h2>
+                                    <p>기사 내용 5</p>
+                                    <span className="source"><strong>연합뉴스</strong></span>
+                                    <span className="date">2024/07/03</span>
+                                </div>
+                                <img src="https://image.wyyyes.com/svc/openChat/NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g/thumbnail/559947618_NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g_f7beddce-af7c-45b2-a17d-7c44fafcbae1.jpg" alt="기사 이미지"/>
+                            </div>
+                        </div>
+                        <div className="box-container">
+                            <div className="article-container">
+                                <div className="content">
+                                    <h2>기사 제목 6</h2>
+                                    <p>기사 내용 6</p>
+                                    <span className="source"><strong>KBS</strong></span>
+                                    <span className="date">2024/07/03</span>
+                                </div>
+                                <img src="https://image.wyyyes.com/svc/openChat/NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g/thumbnail/559947618_NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g_f7beddce-af7c-45b2-a17d-7c44fafcbae1.jpg" alt="기사 이미지"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right-column">
+                        <div className="box-container">
+                            <div className="article-container">
+                                <div className="content">
+                                    <h2>기사 제목 7</h2>
+                                    <p>기사 내용 7</p>
+                                    <span className="source"><strong>MBC</strong></span>
+                                    <span className="date">2024/07/03</span>
+                                </div>
+                                <img src="https://image.wyyyes.com/svc/openChat/NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g/thumbnail/559947618_NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g_f7beddce-af7c-45b2-a17d-7c44fafcbae1.jpg" alt="기사 이미지"/>
+                            </div>
+                        </div>
+                        <div className="box-container">
+                            <div className="article-container">
+                                <div className="content">
+                                    <h2>기사 제목 8</h2>
+                                    <p>기사 내용 8</p>
+                                    <span className="source"><strong>SBS</strong></span>
+                                    <span className="date">2024/07/03</span>
+                                </div>
+                                <img src="https://image.wyyyes.com/svc/openChat/NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g/thumbnail/559947618_NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g_f7beddce-af7c-45b2-a17d-7c44fafcbae1.jpg" alt="기사 이미지"/>
+                            </div>
+                        </div>
+                        <div className="box-container">
+                            <div className="article-container">
+                                <div className="content">
+                                    <h2>기사 제목 9</h2>
+                                    <p>기사 내용 9</p>
+                                    <span className="source"><strong>YTN</strong></span>
+                                    <span className="date">2024/07/03</span>
+                                </div>
+                                <img src="https://image.wyyyes.com/svc/openChat/NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g/thumbnail/559947618_NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g_f7beddce-af7c-45b2-a17d-7c44fafcbae1.jpg" alt="기사 이미지"/>
+                            </div>
+                        </div>
+                        <div className="box-container">
+                            <div className="article-container">
+                                <div className="content">
+                                    <h2>기사 제목 10</h2>
+                                    <p>기사 내용 10</p>
+                                    <span className="source"><strong>연합뉴스</strong></span>
+                                    <span className="date">2024/07/03</span>
+                                </div>
+                                <img src="https://image.wyyyes.com/svc/openChat/NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g/thumbnail/559947618_NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g_f7beddce-af7c-45b2-a17d-7c44fafcbae1.jpg" alt="기사 이미지"/>
+                            </div>
+                        </div>
+                        <div className="box-container">
+                            <div className="article-container">
+                                <div className="content">
+                                    <h2>기사 제목 11</h2>
+                                    <p>기사 내용 11</p>
+                                    <span className="source"><strong>KBS</strong></span>
+                                    <span className="date">2024/07/03</span>
+                                </div>
+                                <img src="https://image.wyyyes.com/svc/openChat/NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g/thumbnail/559947618_NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g_f7beddce-af7c-45b2-a17d-7c44fafcbae1.jpg" alt="기사 이미지"/>
+                            </div>
+                        </div>
+                        <div className="box-container">
+                            <div className="article-container">
+                                <div className="content">
+                                    <h2>기사 제목 12</h2>
+                                    <p>기사 내용 12</p>
+                                    <span className="source"><strong>MBC</strong></span>
+                                    <span className="date">2024/07/03</span>
+                                </div>
+                                <img src="https://image.wyyyes.com/svc/openChat/NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g/thumbnail/559947618_NAFwglGqlAY6_rucDSg78BP2dfyrtYCHmMOj6qxPrf81g_f7beddce-af7c-45b2-a17d-7c44fafcbae1.jpg" alt="기사 이미지"/>
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </main>
         </div>
